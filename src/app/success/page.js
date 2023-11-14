9c1bb85a32385631d8830b638c96be80d35aa773
Vedant Gour <vedantgour45@gmail.com>
2023-11-14 14:19:53 +0530

    test

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const SuccessPage = () => {
   return (
-    <div>SuccessPage</div>
-  )
-}
+    <div>
+      <h1>Success!</h1>
+    </div>
+  );
+};
 
-export default SuccessPage
+export default SuccessPage;
